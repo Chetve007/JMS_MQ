937ba60353cce18f9345a3d0c4d3fa634888ae27
Aleksey <chetve007@mail.ru>
2018-10-03 21:35:02 +0300

    add changes to class

--- src/main/java/MQclass.java
+++ src/main/java/MQclass.java
@@ -5,56 +5,69 @@ import javax.jms.*;
 
 public class MQclass {
 
-    String host = "localhost";        // хост, где расположен MQ-сервер
-    String port = "1414";        // порт для работы с MQ-сервером
-    String mqQManager = "api_example"; // менеджер очередей MQ
-    String mqQChannel = "SYSTEM.DEF.SVRCONN";    // Канал для подключения к MQ-серверу
-    String mqQIn = "MQ.Incoming";    // Очередь входящих сообщений
-    String mqQOut = "MQ.Outgoing";    // Очередь исходящих сообщений
+    static MQQueueConnection mqConn;
+    static MQQueueConnectionFactory mqCF;
+    static MQQueueSession mqQSession;
+    static MQQueue mqIn;
+    static MQQueue mqOut;
+    static MQQueueSender mqSender;
+    static MQQueueReceiver mqReceiver;
+    static MessageProducer replyProd;
 
-    MQQueueConnection mqConn;
-    MQQueueConnectionFactory mqCF = new MQQueueConnectionFactory();
-    MQQueueSession mqQSession;
-    MQQueue mqIn;
-    MQQueue mqOut;
-    MQQueueSender mqSender;
-    MQQueueReceiver mqReceiver;
-    MessageProducer replyProd;
+    public static void main(String[] args) {
 
-    public void mqSettings() {
-        mqCF.setHostName(host);
-        try {
-            mqCF.setPort(Integer.parseInt(port));
-            mqCF.setQueueManager(mqQManager);
-            mqCF.setChannel(mqQChannel);
-            mqCF.setTransportType(WMQConstants.TIME_TO_LIVE_UNLIMITED);
+        Thread threadOne = new Thread(() -> {
+            String host = "localhost";        // хост, где расположен MQ-сервер
+            String port = "1414";        // порт для работы с MQ-сервером
+            String mqQManager = "api_example"; // менеджер очередей MQ
+            String mqQChannel = "SYSTEM.DEF.SVRCONN";    // Канал для подключения к MQ-серверу
+            String mqQIn = "Hello";    // Очередь входящих сообщений
+            String mqQOut = "Bye";    // Очередь исходящих сообщений
 
-            mqConn = (MQQueueConnection) mqCF.createQueueConnection();
-            mqQSession = (MQQueueSession) mqConn.createQueueSession(true, Session.AUTO_ACKNOWLEDGE);
+            try {
+                mqCF = new MQQueueConnectionFactory();
+                mqCF.setHostName(host);
+                mqCF.setPort(Integer.parseInt(port));
+                mqCF.setQueueManager(mqQManager);
+                mqCF.setChannel(mqQChannel);
+                mqCF.setTransportType(WMQConstants.TIME_TO_LIVE_UNLIMITED);
 
-            mqIn = (MQQueue) mqQSession.createQueue("SomeInQueue"); // входная
-            mqOut = (MQQueue) mqQSession.createQueue("SomeOutQueue"); // выходная
+                mqConn = (MQQueueConnection) mqCF.createQueueConnection();
+                mqQSession = (MQQueueSession) mqConn.createQueueSession(true, Session.AUTO_ACKNOWLEDGE);
 
-            mqSender = (MQQueueSender) mqQSession.createSender(mqOut);
-            mqReceiver = (MQQueueReceiver) mqQSession.createReceiver(mqIn);
-            this.replyProd = this.mqQSession.createProducer(null);
-            this.replyProd.setDeliveryMode(DeliveryMode.NON_PERSISTENT);
-            mqConn.start();
-        } catch (JMSException e) {
-            e.printStackTrace();
-        }
+                mqIn = (MQQueue) mqQSession.createQueue("SomeInQueue"); // входная
+                mqOut = (MQQueue) mqQSession.createQueue("SomeOutQueue"); // выходная
+
+                mqSender = (MQQueueSender) mqQSession.createSender(mqOut);
+                mqReceiver = (MQQueueReceiver) mqQSession.createReceiver(mqIn);
+                replyProd = mqQSession.createProducer(null);
+                replyProd.setDeliveryMode(DeliveryMode.NON_PERSISTENT);
+
+                mqConn.start();
+
+                mqReceiver.setMessageListener(new MessageListener() {
+                    @Override
+                    public void onMessage(Message msg) {
+                        if (msg instanceof TextMessage) {
+                            TextMessage tMsg = (TextMessage) msg;
+                            try {
+                                String msgText = ((TextMessage) msg).getText();
+                                mqQSession.commit();
+                                sendAnswer(msg);
+                            } catch (JMSException e) {
+                                e.printStackTrace();
+                            }
+                        }
+                    }
+                });
+            } catch (JMSException e) {
+                e.printStackTrace();
+            }
+        });
+        threadOne.start();
     }
 
-    MessageListener listener = msg -> {
-        if (msg instanceof TextMessage) {
-            TextMessage tMsg = (TextMessage) msg;
-            try { String msgText = ((TextMessage) msg).getText();
-            } catch (JMSException e) { e.printStackTrace(); }
-        }
-
-    };
-
-    public void sendAns(Message msg) {
+    public static void sendAnswer(Message msg) {
         String xmlAns = "Something";
         try {
             TextMessage answer = mqQSession.createTextMessage(xmlAns); //xmlAns – ответная xml
